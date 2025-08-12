b393728cf11fbab9291a83720f5b90d10c63ae25
Remybeccat <115540114+Remybeccat@users.noreply.github.com>
2025-08-12 16:04:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import datetime
+from datetime import datetime, timedelta
 import altair as alt
 from opencage.geocoder import OpenCageGeocode
 
