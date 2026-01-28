965abb96bf45513f1423d52b369c0302300a530f
Remybeccat <115540114+Remybeccat@users.noreply.github.com>
2026-01-28 10:04:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,7 +134,8 @@ if address:
                 st.dataframe(nearby_stations)
             # Sélection station
             selected_station_name = st.selectbox("Sélectionnez une station :", nearby_stations["name"].tolist())
-            selected_station_id = nearby_stations.loc["id",selected_station_name]
+            selected_station_id = nearby_stations.loc[selected_station_name]
+            st.write(selected_station_id)
             
             year_max = datetime.date.today().year
             start_date_FR = st.date_input(
